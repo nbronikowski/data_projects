f82aefd3d4fdff3d4cdec47f77867b2603d78434
Nicolai von Oppeln-Bronikowski <57228182+nbronikowski@users.noreply.github.com>
2023-05-18 18:16:14 -0230

    cleanup

---
+++ TreeCore_ImageStitching/image_files/test3/stich_script.m
@@ -0,0 +1,123 @@
+clear; clear all;
+imageDir = fullfile('*.jpg');
+buildingScene = imageDatastore(imageDir);
+% montage(buildingScene.Files)
+
+I = readimage(buildingScene,1);
+
+% Initialize features for I(1)
+grayImage = im2gray(I);
+points = detectSURFFeatures(grayImage);
+[features, points] = extractFeatures(grayImage,points);
+
+numImages = numel(buildingScene.Files);
+tforms(numImages) = projective2d;
+
+% Initialize variable to hold image sizes.
+imageSize = zeros(numImages,2);
+
+% Iterate over remaining image pairs
+for n = 2:numImages
+    % Store points and features for I(n-1).
+    pointsPrevious = points;
+    featuresPrevious = features;
+        
+    % Read I(n).
+    I = readimage(buildingScene, n);
+    
+    % Convert image to grayscale.
+    grayImage = im2gray(I);    
+    
+    % Save image size.
+    imageSize(n,:) = size(grayImage);
+    
+    % Detect and extract SURF features for I(n).
+    points = detectSURFFeatures(grayImage);    
+    [features, points] = extractFeatures(grayImage, points);
+  
+    % Find correspondences between I(n) and I(n-1).
+    indexPairs = matchFeatures(features, featuresPrevious, 'Unique', true);
+       
+    matchedPoints = points(indexPairs(:,1), :);
+    matchedPointsPrev = pointsPrevious(indexPairs(:,2), :);        
+    
+    % Estimate the transformation between I(n) and I(n-1).
+    tforms(n) = estimateGeometricTransform2D(matchedPoints, matchedPointsPrev,...
+        'projective', 'Confidence', 99.9, 'MaxNumTrials', 1000);
+    
+    % Compute T(1) * T(2) * ... * T(n-1) * T(n).
+    tforms(n).T = tforms(n-1).T * tforms(n).T; 
+end
+
+% Compute the output limits for each transformation.
+for i = 1:numel(tforms)           
+    [xlim(i,:), ylim(i,:)] = outputLimits(tforms(i), [1 imageSize(i,2)], [1 imageSize(i,1)]);    
+end
+
+% Next, compute the average X limits for each transformation and find the
+% image that is in the center. Only the X limits are used here because the
+% scene is known to be horizontal. If another set of images are used, both
+% the X and Y limits may need to be used to find the center image.
+
+avgXLim = mean(xlim, 2);
+avgYLim = mean(ylim, 2);
+[~,idx] = sort(avgXLim);
+[~,idy] = sort(avgYLim);
+
+centerIdx = floor((numel(tforms)+1)/2);
+centerImageIdx = idx(centerIdx);
+
+Tinv = invert(tforms(centerImageIdx));
+for i = 1:numel(tforms)    
+    tforms(i).T = double(Tinv.T * tforms(i).T);
+end
+
+
+%% init pano
+for i = 1:numel(tforms)           
+    [xlim(i,:), ylim(i,:)] = outputLimits(tforms(i), [1 imageSize(i,2)], [1 imageSize(i,1)]);
+end
+
+maxImageSize = max(imageSize);
+
+% Find the minimum and maximum output limits. 
+xMin = min([1; xlim(:)]);
+xMax = max([maxImageSize(2); xlim(:)]);
+
+yMin = min([1; ylim(:)]);
+yMax = max([maxImageSize(1); ylim(:)]);
+
+% Width and height of panorama.
+width  = round(xMax - xMin);
+height = round(yMax - yMin);
+
+% Initialize the "empty" panorama.
+panorama = zeros([height width 3], 'like', I);
+
+%% create
+blender = vision.AlphaBlender('Operation', 'Binary mask', ...
+    'MaskSource', 'Input port');  
+
+% Create a 2-D spatial reference object defining the size of the panorama.
+xLimits = [xMin xMax];
+yLimits = [yMin yMax];
+panoramaView = imref2d([height width], xLimits, yLimits);
+
+% Create the panorama.
+for i = 1:numImages
+    
+    I = readimage(buildingScene, i);   
+   
+    % Transform I into the panorama.
+    warpedImage = imwarp(I, tforms(i), 'OutputView', panoramaView);
+                  
+    % Generate a binary mask.    
+    mask = imwarp(true(size(I,1),size(I,2)), tforms(i), 'OutputView', panoramaView);
+    
+    % Overlay the warpedImage onto the panorama.
+    panorama = step(blender, panorama, warpedImage, mask);
+end
+
+% figure
+imshow(panorama)
+imwrite(panorama,'output.png')
