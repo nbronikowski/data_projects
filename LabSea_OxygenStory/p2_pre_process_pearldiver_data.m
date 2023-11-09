24fdd6dff5d00b1e394ec0b236cd51423353e3cf
Nicolai von Oppeln-Bronikowski <57228182+nbronikowski@users.noreply.github.com>
2023-11-09 13:58:47 -0330

    oxy updates

--- LabSea_OxygenStory/p2_pre_process_pearldiver_data.m
+++ LabSea_OxygenStory/p2_pre_process_pearldiver_data.m
@@ -4,7 +4,7 @@ path_name = './mat_files/'
 var_name  = 'pearldiver_data'
 load(fullfile(path_name,[var_name,'.mat']))
 
-%% Basic Cleaning (not really scientific)
+%% basic QC
 
 % compute datetime stamps
 pearldiver.time = datetime(pearldiver.time,'ConvertFrom','posixtime');
@@ -13,6 +13,19 @@ pearldiver.time = datetime(pearldiver.time,'ConvertFrom','posixtime');
 idbad = (pearldiver.temperature<1) | (pearldiver.conductivity<1);
 pearldiver.temperature(idbad)=NaN;
 pearldiver.conductivity(idbad)=NaN;
+
+% remove large conductivity spikes
+idnan = find(~isnan(pearldiver.conductivity));
+idSpike = abs(sgolayfilt(pearldiver.conductivity(idnan),3,11)-pearldiver.conductivity(idnan))>0.04;
+
+% figure();
+% hold on
+% plot(pearldiver.time(idnan),pearldiver.conductivity(idnan));
+% plot(pearldiver.time(idnan(idSpike)),pearldiver.conductivity(idnan(idSpike)),'*m')
+
+pearldiver.conductivity(idnan(idSpike))=NaN;
+pearldiver.temperature(idnan(idSpike))=NaN;
+pearldiver.pressure(idnan(idSpike))=NaN;
 
 % oxygen calphase has a lot of spikes
 idbad = (pearldiver.oxygen_calphase<33) | isnan(pearldiver.temperature);
@@ -25,22 +38,58 @@ temp = pearldiver.oxygen_calphase(~id);
 windowSize = 15; floor(length(temp(~isnan(temp)))/1000); %
 rollingStd = movstd(temp, windowSize);
 rollingMean = movmean(temp, windowSize);
-idSpikes = abs(temp - rollingMean) > 2 * rollingStd; % k is a threshold, like 2 or 3
-temp(idSpikes) = NaN;
+idSpike = abs(temp - rollingMean) > 2 * rollingStd; % k is a threshold, like 2 or 3
+temp(idSpike) = NaN;
 pearldiver.oxygen_calphase_cleaned = nan*pearldiver.oxygen_calphase;
 pearldiver.oxygen_calphase_cleaned(~id)=temp;
+clear temp
 
-% compute raw oxygen levels
-% pearldiver had optode 124
+%% correct salinity - maybe best to break into profile/profile? 
+idnan = find(~(isnan(pearldiver.temperature) | isnan(pearldiver.conductivity)));
+conductivity=pearldiver.conductivity(idnan);
+temperature=pearldiver.temperature(idnan);
+pressure=pearldiver.pressure(idnan);
+
+% Correct salinity lag
+minAdv = -2.5; maxAdv = 2.5;
+[bestAdv,bestSal,advances,goodness,binIndex,salinities] = find_cond_advance(...
+                            temperature, conductivity, pressure,...
+                            minAdv,maxAdv,'showprogress','yes');
+
+% Apply correction to data
+pearldiver.salinity_cor = NaN*pearldiver.salinity;
+pearldiver.salinity_cor(idnan) = bestSal;
+% figure()
+% plot(pearldiver.salinity,'.'); hold on
+% plot(pearldiver.salinity_cor,'.')
+
+%% compute raw oxygen levels
+% pearldiver had Oxygen Optode 4831 SN 124 calibrated last in 2018
 SVUFoilCoef = [2.658710E-03,1.305085E-04,2.536294E-06,6.186505E-02,...
     5.658259E-05,-1.514249E-02,1.192067E-03];
 modeltype ='uchidaAADI';
-pearldiver.raw_oxygen_concentration = optcalcO2(pearldiver.temperature,pearldiver.oxygen_calphase_cleaned,SVUFoilCoef, modeltype,pearldiver.salinity, 1013.25,pearldiver.pressure); 
-
-save(fullfile(path_name,[var_name,'_oxy.mat']),'pearldiver')
-
-%% Oxygen Correction - trying the GEOMAR way
-
-
+pearldiver.raw_oxygen_concentration = optcalcO2(pearldiver.temperature,...
+                    pearldiver.oxygen_calphase_cleaned,SVUFoilCoef,...
+                    modeltype,pearldiver.salinity_cor, 1013.25,...
+                    pearldiver.pressure); 
 
 %% Gridding
+pg = 0:1:ceil(max(pearldiver.pressure,[],'omitnan')); 
+timeDateNum = datenum(pearldiver.time);
+[~,~,time,xu] = pgrid_columns(pearldiver.profile_index,pearldiver.pressure,timeDateNum,pg);
+[~,~,oxygen_raw] = pgrid_columns(pearldiver.profile_index,pearldiver.pressure,pearldiver.raw_oxygen_concentration,pg);
+[~,~,salinity] = pgrid_columns(pearldiver.profile_index,pearldiver.pressure,pearldiver.salinity_cor,pg);
+[~,~,temperature] = pgrid_columns(pearldiver.profile_index,pearldiver.pressure,pearldiver.temperature,pg);
+[~,~,conductivity] = pgrid_columns(pearldiver.profile_index,pearldiver.pressure,pearldiver.conductivity,pg);
+[~,~,pressure] = pgrid_columns(pearldiver.profile_index,pearldiver.pressure,pearldiver.pressure,pg);
+
+[pearldiver.gridded.oxygen_raw,column_idx]=deleteAlmostEmptyColumns(oxygen_raw,pg);
+[pearldiver.gridded.temperature,~]=deleteAlmostEmptyColumns(temperature,pg);
+[pearldiver.gridded.salinity,~]=deleteAlmostEmptyColumns(salinity,pg);
+[pearldiver.gridded.conductivity,~]=deleteAlmostEmptyColumns(conductivity,pg);
+pearldiver.gridded.time = time(:,column_idx);
+pearldiver.gridded.profile_index = xu(column_idx);
+pearldiver.gridded.pressure_grid = pg;
+pearldiver.gridded.pressure = deleteAlmostEmptyColumns(pressure,pg);
+
+save(fullfile(path_name,[var_name,'_clean.mat']),'pearldiver')
--- LabSea_OxygenStory/s2_pre_process_sunfish_data.m
+++ LabSea_OxygenStory/s2_pre_process_sunfish_data.m
@@ -14,12 +14,68 @@ idbad = (sunfish.temperature==1) | (sunfish.conductivity<1);
 sunfish.temperature(idbad)=NaN;
 sunfish.conductivity(idbad)=NaN;
 
-% process oxygen
-% sunfish had optode 673
+% remove large conductivity spikes
+idnan = find(~isnan(sunfish.conductivity));
+idSpike = abs(sgolayfilt(sunfish.conductivity(idnan),3,11)-sunfish.conductivity(idnan))>0.04;
+
+% figure();
+% hold on
+% plot(sunfish.time(idnan),sunfish.conductivity(idnan));
+% plot(sunfish.time(idnan(idSpike)),sunfish.conductivity(idnan(idSpike)),'*m')
+
+sunfish.conductivity(idnan(idSpike))=NaN;
+sunfish.temperature(idnan(idSpike))=NaN;
+sunfish.pressure(idnan(idSpike))=NaN;
+
+%% correct salinity - maybe best to break into profile/profile? 
+idnan = find(~(isnan(sunfish.temperature) | isnan(sunfish.conductivity)));
+conductivity=sunfish.conductivity(idnan);
+temperature=sunfish.temperature(idnan);
+pressure=sunfish.pressure(idnan);
+
+% Correct salinity lag
+minAdv = -2.5; maxAdv = 2.5;
+[bestAdv,bestSal,advances,goodness,binIndex,salinities] = find_cond_advance(...
+                            temperature, conductivity, pressure,...
+                            minAdv,maxAdv,'showprogress','yes');
+% Apply correction to data
+sunfish.salinity_cor = NaN*sunfish.salinity;
+sunfish.salinity_cor(idnan) = bestSal;
+figure()
+plot(sunfish.salinity,'.'); hold on
+plot(sunfish.salinity_cor,'.')
+
+
+%% Compute raw oxygen
+% sunfish had oxygen optode SN 673 with factory calibration from 2017
 SVUFoilCoef = [2.78565E-03,1.17409E-04,2.39332E-06,2.32859E+02,-3.80398E-01,-4.79825E+01,4.62200E+00];
 modeltype ='uchidaAADI';
-sunfish.raw_oxygen_concentration = optcalcO2(sunfish.temperature,sunfish.oxygen_calphase,SVUFoilCoef, modeltype,sunfish.salinity, 1013.25,sunfish.pressure); 
+sunfish.raw_oxygen_concentration = optcalcO2(sunfish.temperature,...
+                sunfish.oxygen_calphase,SVUFoilCoef, modeltype,...
+                sunfish.salinity_cor, 1013.25,sunfish.pressure); 
 
-save(fullfile(path_name,[var_name,'_oxy.mat']),'sunfish')
+
+%% Gridding
+pg = 0:1:ceil(max(sunfish.pressure,[],'omitnan')); 
+timeDateNum = datenum(sunfish.time);
+[~,~,time,xu] = pgrid_columns(sunfish.profile_index,sunfish.pressure,timeDateNum,pg);
+[~,~,oxygen_raw] = pgrid_columns(sunfish.profile_index,sunfish.pressure,sunfish.raw_oxygen_concentration,pg);
+[~,~,salinity] = pgrid_columns(sunfish.profile_index,sunfish.pressure,sunfish.salinity_cor,pg);
+[~,~,temperature] = pgrid_columns(sunfish.profile_index,sunfish.pressure,sunfish.temperature,pg);
+[~,~,conductivity] = pgrid_columns(sunfish.profile_index,sunfish.pressure,sunfish.conductivity,pg);
+[~,~,pressure] = pgrid_columns(sunfish.profile_index,sunfish.pressure,sunfish.pressure,pg);
+
+[sunfish.gridded.temperature,~]=deleteAlmostEmptyColumns(temperature,pg);
+[sunfish.gridded.salinity,~]=deleteAlmostEmptyColumns(salinity,pg);
+[sunfish.gridded.conductivity,column_idx]=deleteAlmostEmptyColumns(conductivity,pg);
+sunfish.gridded.time = time(:,column_idx);
+sunfish.gridded.profile_index = xu(column_idx);
+sunfish.gridded.pressure_grid = pg;
+sunfish.gridded.pressure = deleteAlmostEmptyColumns(pressure,pg);
+sunfish.gridded.oxygen_raw = oxygen_raw(:,column_idx);
+
+
+%% Save result
+save(fullfile(path_name,[var_name,'_clean.mat']),'sunfish')
 
 
